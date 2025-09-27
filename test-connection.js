c77fb2dba94f801a29ca95f8d2d000b817ba978e
yashyash2020 <yashar2010eg@gmail.com>
2025-09-27 20:24:19 +0300

    تحديث متغيرات البيئة بالقيم الصحيحة لقاعدة البيانات

--- test-connection.js
+++ test-connection.js
@@ -1,16 +1,16 @@
 // اختبار اتصال Supabase
-const https = require('https');
+import https from 'https';
 
-const supabaseUrl = 'https://fpcnnndjofzwqmkrnujo.supabase.co';
-const serviceRoleKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwY25ubmRqb2Z6d3Fta3JudWpvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjc4OTYwMiwiZXhwIjoyMDQyMzY1NjAyfQ.fWKlb06qUyHkPzEQ6ynfVvQ3SJmFVDBMf66SBjKVbYo';
+const supabaseUrl = 'https://imobhmzywvzbvyqpzcau.supabase.co';
+const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imltb2JobXp5d3Z6YnZ5cXB6Y2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4NDkzNTEsImV4cCI6MjA3MjQyNTM1MX0.FpUXi86I-o38ecc0S1eJ6E2o1TRgYP-yNmOHqyYO3Pg';
 
 console.log('🔍 اختبار الاتصال مع Supabase...');
 
 // اختبار بسيط للاتصال
 https.get(`${supabaseUrl}/rest/v1/assessment_tools?select=id,code&limit=1`, {
     headers: {
-        'apikey': serviceRoleKey,
-        'Authorization': `Bearer ${serviceRoleKey}`,
+        'apikey': anonKey,
+        'Authorization': `Bearer ${anonKey}`,
         'Content-Type': 'application/json'
     }
 }, (res) => {
